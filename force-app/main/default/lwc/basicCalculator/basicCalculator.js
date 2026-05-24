42e29228068a15689f0ca03e80d0522444d71484
Rohit Thakur <thakurrohit853@gmail.com>
2026-05-25 01:26:25 +0530

    Added new Lightning Web Component

--- force-app/main/default/lwc/basicCalculator/basicCalculator.js
+++ force-app/main/default/lwc/basicCalculator/basicCalculator.js
@@ -1,3 +1,32 @@
 import { LightningElement } from 'lwc';
 
-export default class BasicCalculator extends LightningElement {}
+export default class BasicCalculator extends LightningElement {
+
+    firstInput;
+    secondInput;
+    totalValue; 
+    showResult;
+    firstInputChange(event){
+        this.firstInput =  event.target.value;   
+    } 
+    secondInputChange(event){
+        this.secondInput = event.target.value;
+    }  
+    add(event){
+        this.totalValue = Number(this.firstInput) + Number(this.secondInput); 
+        ths.showResult = true;
+    }
+    subtract(event){
+        this.totalValue = Number(this.firstInput) - Number(this.secondInput); 
+        ths.showResult = true;
+    }
+    multipication(event){
+        this.totalValue = Number(this.firstInput) * Number(this.secondInput); 
+        ths.showResult = true;
+    }
+    div(event){
+        this.totalValue = Number(this.firstInput) / Number(this.secondInput); 
+        ths.showResult = true;
+    }
+}
+
