9f400e7da24e64d77f352a58af7e70b6bb3ee8bf
Rohit Thakur <thakurrohit853@gmail.com>
2026-05-26 17:19:51 +0530

    Added new Lightning Web ComponentIterator

---
+++ force-app/main/default/lwc/allCourseIterator/allCourseIterator.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+
+export default class AllCourseIterator extends LightningElement {
+    allCourseDetails =[
+        {courseName:'LWC',courseDuration:'20Days'},
+        {courseName:'Integration',courseDuration:'30Days'},
+        {courseName:'Aura Componnet',courseDuration:'20Days'},
+        {courseName:'Design pattern',courseDuration:'30Days'}
+    ];
+}
