f3a864b729a48980c0a05183c1b0cf14aa5c85d7
Rohit Thakur <thakurrohit853@gmail.com>
2026-05-26 21:04:08 +0530

    Sample code Use Navigation service to Navigate to Web page also Public property use in lightning web component

--- force-app/main/default/lwc/enrollCourse/enrollCourse.js
+++ force-app/main/default/lwc/enrollCourse/enrollCourse.js
@@ -1,6 +1,15 @@
 import { LightningElement, api } from 'lwc';
-
-export default class EnrollCourse extends LightningElement {
-     @api courseDetailInfo=
-     {courseName:'Lightning Web Component',courseDuration:'30 Days',courseFee:'10,000', courseRating:'*****'}
+import { NavigationMixin } from 'lightning/navigation';
+export default class EnrollCourse  extends NavigationMixin(LightningElement){
+    @api courseDetailInfo={courseName:'Lightning Web Component',courseDuration:'30 Days',courseFee:'10,000', courseRating:'*****'}
+    NavigateToCourse(){
+        this[NavigationMixin.Navigate]({
+            type:'standard__webPage',
+            attributes:{
+                url:'https://www.udemy.com/courses/search/?src=ukw&q=debasis+jena'
+            }
+           
+ 
+        });
+    }
 }
